367855b15698eeeef4206a1b7a56b28d94bc2804
sterpe <srterpe@gmail.com>
2015-09-27 16:22:35 -0700

    clean up code a bit

--- javascript/k-length-rotational-symmetric-values.js
+++ javascript/k-length-rotational-symmetric-values.js
@@ -7,30 +7,41 @@ K[1] = [ '0', '1', '8' ];
 K[2] = [ '00', '11', '69', '88', '96' ];
 
 module.exports = function ƒ(k) {
-	if (k < 3) {
+	if (k < 3 || K[k]) {
 		return K[k];
 	}
 	let i = 3;
 	let j = k + 1;
 
-	for (; i < j; ++i) {
-		let q = 0;
-		let r = 0;
-		K[i] = [ ];
-		if (i % 2 !== 0) {
+	while (K[i]) {
+		i++;
+	}
 
-			for (; q < K[i - 1].length; ++q, r = 0) {
-				for (; r < K[1].length; ++r) {
-					K[i].push(K[i - 1][q].slice(0, (K[i - 1][q].length/2)) + K[1][r] + K[i -1][q].slice((K[i - 1][q].length/2)));
-				}
-			}
+	for (; i < j; ++i) {
+		let q, r, x, y, g;
+
+		K[i] = [ ];
+
+		if (i % 2 !== 0) {
+			x = i - 1;
+			y = 1;
+			g = function (x, y, q, r) {
+				return K[x][q].slice(0, K[x][q].length/2) + K[y][r] + K[x][q].slice(K[x][q].length/2);
+			};
 		} else {
-			for (; q < K[i - 2].length; ++q, r = 0) {
-				for (; r < K[2].length; ++r) {
-					K[i].push(K[2][r][0] + K[i - 2][q] + K[2][r][1]);
-				}
+			x = i - 2;
+			y = 2;
+			g = function (x, y, q, r) {
+				return K[y][r][0] + K[x][q] + K[y][r][1];
+			};
+		}
+
+		for (q = 0; q < K[x].length; ++q) {
+			for (r = 0; r < K[y].length; ++r) {
+				K[i].push(g(x, y, q, r));
 			}
 		}
 	}
+
 	return K[k];
-};
+}
